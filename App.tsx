ba361f8c301b75fa730aba509b1e6feb9a870c89
Gabriel Coelho <coelhogabriel67@gmail.com>
2022-07-31 19:26:59 -0300

    feat(global): first commit

--- App.tsx
+++ App.tsx
@@ -1,22 +1,15 @@
-import React from "react";
-import { StatusBar } from "react-native";
-import { GestureHandlerRootView } from "react-native-gesture-handler";
+import React from 'react'
+import {StatusBar, Text, View} from 'react-native'
 
 const App = () => {
-	return (
-		<>
-			<GestureHandlerRootView style={{ flex: 1 }}>
-				<StatusBar
-					backgroundColor={"transparent"}
-					translucent
-					barStyle={"light-content"}
-				/>
-				<View>
-					<Text>Hello World!</Text>
-				</View>
-			</GestureHandlerRootView>
-		</>
-	);
-};
+  return (
+    <>
+      <StatusBar backgroundColor={'transparent'} translucent barStyle={'light-content'} />
+      <View>
+        <Text>Hello World!</Text>
+      </View>
+    </>
+  )
+}
 
-export default App;
+export default App
